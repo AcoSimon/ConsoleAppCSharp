8861da9de079c4b89f9f5682638346fa94d2b920
Acosta Simon <108154682+AcoSimon@users.noreply.github.com>
2023-03-30 19:24:18 -0300

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,74 +1,75 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace ConsoleApp
+class Program
 {
-    internal static class calculadora
+    static void Main()
     {
-        static void Main()
+        Console.WriteLine("Calculadora básica");
+        Console.WriteLine("------------------");
+
+        double num1, num2;
+        Console.Write("\a Introduce el primer número: \a");
+        while (!double.TryParse(Console.ReadLine(), out num1))
         {
-            Console.WriteLine("Calculadora básica");
-            Console.WriteLine("------------------");
+            Console.WriteLine("Introduce un número válido.");
+            Console.Write("\a Introduce el primer número: \a");
+        }
 
-            double num1, num2;
-            Console.Write("Introduce el primer número: ");
-            while (!double.TryParse(Console.ReadLine(), out num1))
-            {
-                Console.WriteLine("Introduce un número válido.");
-                Console.Write("Introduce el primer número: ");
-            }
-
+        Console.Write("\a Introduce el segundo número: \a");
+        while (!double.TryParse(Console.ReadLine(), out num2))
+        {
+            Console.WriteLine("Introduce un número válido.");
             Console.Write("Introduce el segundo número: ");
-            while (!double.TryParse(Console.ReadLine(), out num2))
-            {
-                Console.WriteLine("Introduce un número válido.");
-                Console.Write("Introduce el segundo número: ");
-            }
+        }
 
-            Console.WriteLine("Selecciona la operación a realizar:");
-            Console.WriteLine("\t1. Suma (+)");
-            Console.WriteLine("\t2. Resta (-)");
-            Console.WriteLine("\t3. Multiplicación (*)");
-            Console.WriteLine("\t4. División (/)");
+        Console.WriteLine("\a Selecciona la operación a realizar: \a");
+        Console.WriteLine("\t1. Suma (+)");
+        Console.WriteLine("\t2. Resta (-)");
+        Console.WriteLine("\t3. Multiplicación (*)");
+        Console.WriteLine("\t4. División (/)");
 
-            Console.Write("Escribe el número de la operación: ");
-            int opcion;
-            while (!int.TryParse(Console.ReadLine(), out opcion) || opcion < 1 || opcion > 4)
-            {
-                Console.WriteLine("Introduce una opción válida (1-4).");
-                Console.Write("Escribe el número de la operación: ");
-            }
+        Console.Write("\a Escribe el número de la operación: \a");
+        int opcion;
+        while (!int.TryParse(Console.ReadLine(), out opcion) || opcion < 1 || opcion > 4)
+        {
+            Console.WriteLine("Introduce una opción válida (1-4).");
+            Console.Write("\a Escribe el número de la operación: \a");
+        }
 
-            double resultado = 0;
+        double resultado = 0;
 
-            switch (opcion)
-            {
-                case 1:
-                    resultado = num1 + num2;
-                    break;
-                case 2:
-                    resultado = num1 - num2;
-                    break;
-                case 3:
-                    resultado = num1 * num2;
-                    break;
-                case 4:
-                    if (num2 == 0)
+        switch (opcion)
+        {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                if (num2 == 0)
+                {
+                    if (num2 == 0 && num1 == 0)
                     {
-                        Console.WriteLine("No se puede dividir entre 0");
+                        Console.WriteLine("\nNo valido. {Undefined}");
                         return;
                     }
-                    resultado = num1 / num2;
-                    break;
-                default:
-                    Console.WriteLine("Opción no válida");
-                    return;
-            }
-
-            Console.WriteLine("El resultado de la operación es: " + resultado);
+                    else
+                    {
+                        Console.WriteLine("No se puede dividir entre 0.");
+                        return;
+                    }
+                }
+                resultado = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Opción no válida");
+                return;
         }
-    }
 
+        Console.WriteLine("El resultado de la operación es: " + resultado+"\n\n");
+    }
 }
